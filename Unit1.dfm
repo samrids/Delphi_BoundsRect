b7fdd5318bb3d3e7ee08f708a29f2e215576d500
Samrid Somboon <samrids@gmail.com>
2022-09-25 08:24:47 +0700

    first commit

---
+++ Unit1.dfm
@@ -0,0 +1,117 @@
+object Form1: TForm1
+  Left = 0
+  Top = 0
+  Caption = 'Form1'
+  ClientHeight = 566
+  ClientWidth = 863
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Label1: TLabel
+    Left = 64
+    Top = 185
+    Width = 36
+    Height = 13
+    Caption = 'Label1'
+  end
+  object Label2: TLabel
+    Left = 64
+    Top = 56
+    Width = 31
+    Height = 13
+    Caption = 'Label2'
+  end
+  object Label4: TLabel
+    Left = 64
+    Top = 88
+    Width = 31
+    Height = 13
+    Caption = 'Label4'
+  end
+  object Label3: TLabel
+    Left = 64
+    Top = 128
+    Width = 31
+    Height = 13
+    Caption = 'Label3'
+  end
+  object Label5: TLabel
+    Left = 64
+    Top = 147
+    Width = 31
+    Height = 13
+    Caption = 'Label5'
+  end
+  object Label6: TLabel
+    Left = 64
+    Top = 166
+    Width = 31
+    Height = 13
+    Caption = 'Label6'
+  end
+  object Label7: TLabel
+    Left = 130
+    Top = 57
+    Width = 31
+    Height = 13
+    Caption = 'Label7'
+  end
+  object Label8: TLabel
+    Left = 128
+    Top = 76
+    Width = 31
+    Height = 13
+    Caption = 'Label8'
+  end
+  object Label9: TLabel
+    Left = 130
+    Top = 95
+    Width = 31
+    Height = 13
+    Caption = 'Label9'
+  end
+  object Label10: TLabel
+    Left = 130
+    Top = 114
+    Width = 37
+    Height = 13
+    Caption = 'Label10'
+  end
+  object Label11: TLabel
+    Left = 130
+    Top = 133
+    Width = 37
+    Height = 13
+    Caption = 'Label11'
+  end
+  object Label12: TLabel
+    Left = 130
+    Top = 152
+    Width = 37
+    Height = 13
+    Caption = 'Label12'
+  end
+  object Panel1: TPanel
+    Left = 230
+    Top = 128
+    Width = 563
+    Height = 385
+    Caption = 'Parent'
+    TabOrder = 0
+  end
+  object Button1: TButton
+    Left = 752
+    Top = 32
+    Width = 75
+    Height = 25
+    Caption = 'Button1'
+    TabOrder = 1
+    OnClick = Button1Click
+  end
+end
---
+++ Unit1.pas
@@ -0,0 +1,85 @@
+﻿unit Unit1;
+{
+Delphi Code Stuff :
+Move TLabel to the border of Parent component.
+}
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ExtCtrls;
+
+type
+  TForm1 = class(TForm)
+    Panel1: TPanel;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label4: TLabel;
+    Label3: TLabel;
+    Label5: TLabel;
+    Label6: TLabel;
+    Label7: TLabel;
+    Label8: TLabel;
+    Label9: TLabel;
+    Label10: TLabel;
+    Label11: TLabel;
+    Label12: TLabel;
+    Button1: TButton;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+//Move Label to border of Panel1 (work with Left and Top)
+//and BoundsRect of Parent
+Label1.Left := Panel1.BoundsRect.Right;
+Label1.Top  := Panel1.BoundsRect.Top+((Panel1.BoundsRect.Bottom - Panel1.BoundsRect.Top) - Label1.Height) div  2;
+
+Label2.Left := Panel1.BoundsRect.Left - Label2.Width;
+Label2.Top := Panel1.Top+((Panel1.BoundsRect.Bottom - Panel1.BoundsRect.Top)-Label2.Height) div 2;
+
+Label3.Left := Panel1.Left+((Panel1.BoundsRect.Right - Panel1.BoundsRect.Left)-Label3.Width) div 2;
+Label3.Top := Panel1.Top - Label3.Height;
+
+Label4.Left := Panel1.Left+((Panel1.BoundsRect.Right - Panel1.BoundsRect.Left)-Label4.Width) div 2;
+Label4.Top := Panel1.BoundsRect.Bottom;
+
+Label5.Left := Panel1.BoundsRect.Left;
+Label5.Top := Panel1.Top - Label5.Height;
+
+Label6.Left := Panel1.BoundsRect.Left - Label6.Width;
+Label6.Top := Panel1.BoundsRect.Top;
+
+Label7.Left := Panel1.BoundsRect.Right - Label7.Width;
+Label7.Top := Panel1.Top - Label7.Height;
+
+Label8.Left := Panel1.BoundsRect.Right;
+Label8.Top := Panel1.Top;
+
+
+Label9.Left := Panel1.BoundsRect.Right;
+Label9.Top := Panel1.BoundsRect.Bottom - Label9.Height;
+
+Label10.Left := Panel1.BoundsRect.Right - Label10.Width;
+Label10.Top := Panel1.BoundsRect.Bottom;
+
+
+Label11.Left := Panel1.Left - Label11.Width;
+Label11.Top := Panel1.BoundsRect.Bottom - Label11.Height;
+
+Label12.Left := Panel1.Left;
+Label12.Top  := Panel1.BoundsRect.Bottom;
+end;
+
+end.
